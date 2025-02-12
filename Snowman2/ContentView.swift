b9bbe036692488d536d7470f9a560276d5267176
smyang0220 <smyang0220@naver.com>
2025-02-13 03:54:19 +0900

    Initial Commit

---
+++ Snowman2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Snowman2
+//
+//  Created by 양희태 on 2/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Snowman2/Snowman2App.swift
@@ -0,0 +1,17 @@
+//
+//  Snowman2App.swift
+//  Snowman2
+//
+//  Created by 양희태 on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Snowman2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
